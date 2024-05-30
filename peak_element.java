c82f17962f470f881f98eca5e6c100499d27dc02
Aditi Machhar <60147219+ajanmcr@users.noreply.github.com>
2024-05-30 12:12:48 +0530

    Create peak_element.java

---
+++ peak_element.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/find-peak-element/
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int start = 0, end = nums.length - 1, mid;
+        while(start < end) {
+            mid = start + (end - start) / 2;
+            //can also reverse the condition like nums[min] < nums[mid+1] and update the value of start
+            //and return start since it'll hold the index of peak element 
+            if(nums[mid] > nums[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return end;
+    }
+}
