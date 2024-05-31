51033b864fa57634858ddf3cd61e0f5005225ad0
Aditi Machhar <60147219+ajanmcr@users.noreply.github.com>
2024-05-31 12:19:42 +0530

    Update KthSmallesAndLargest.java

--- KthSmallesAndLargest.java
+++ KthSmallesAndLargest.java
@@ -5,7 +5,6 @@ import java.util.Comparator;
 
 public class Solution {
 	public static ArrayList<Integer> kthSmallLarge(ArrayList<Integer> arr, int n, int k) {
-		//Write your code here
 		arr.sort(Comparator.naturalOrder());
 		ArrayList<Integer> pair = new ArrayList<>();
 		pair.add(arr.get(k-1));
