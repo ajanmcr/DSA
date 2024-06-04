47994e06245e42151e050aa0d72227650a5cd52b
Aditi Machhar <60147219+ajanmcr@users.noreply.github.com>
2024-06-04 23:09:35 +0530

    Update ShuffleTheArray.java

--- ShuffleTheArray.java
+++ ShuffleTheArray.java
@@ -2,7 +2,9 @@ class Solution {
     public int[] shuffle(int[] nums, int n) {
         int[] ans = new int[2 * n];
         for (int i = 0; i < n; i++) {
+            //even index
             ans[2*i] = nums[i];
+            //odd index
             ans[2*i + 1] = nums[n + i];
         }
         return ans;
